f5707f5f9349bb75fc9daceb857bf058927146cf
Eric Betts <bettse@gmail.com>
2014-10-13 22:48:51 -0700

    webservice for direct device post

--- webservice/app.rb
+++ webservice/app.rb
@@ -45,6 +45,67 @@ class XbmApp < Sinatra::Base
 
   end
 
+  get '/:agent/chart' do
+    url = params[:url] || 'https://staging.newrelic.com/public/charts/fAmWyK06eYB'
+    file = Tempfile.new('chart')
+    path = file.path + '.png'
+
+    Screencap::Fetcher.new(url).fetch(
+      output: path,
+      div: '#embedded-chart',
+      width: WIDTH,
+      height: HEIGHT,
+    )
+
+    image = Magick::Image.read(path) do
+      self.background_color = 'white'
+      self.antialias = false
+    end.first
+
+    image.colorspace = Magick::GRAYColorspace
+    image.image_type = Magick::BilevelType
+
+    image = image.resize_to_fit(WIDTH, HEIGHT).extent(WIDTH, HEIGHT)
+
+    #send_image(image, agent)
+    #Give the user the png version
+    content_type 'image/png'
+    image.format = 'png'
+    image.to_blob
+  end
+
+  post '/text.?:format?' do
+    body = JSON.parse(request.body.read.to_s)
+    msg = body['msg'] || 'hello world'
+    format = params[:format] || 'epd'
+    text = Magick::Draw.new
+
+    image = Magick::Image.new(WIDTH, HEIGHT) do
+      self.colorspace = Magick::GRAYColorspace
+      self.image_type = Magick::BilevelType
+      self.background_color = 'white'
+      self.antialias = false
+    end
+
+    text.annotate(image, WIDTH, HEIGHT, 0, 0, msg) do
+      self.pointsize = 36
+      self.font = "Helvetica"
+      self.font_weight = Magick::BoldWeight
+      self.gravity = Magick::CenterGravity
+      self.fill = 'black'
+    end
+
+    if format == 'png'
+      #Give the user the png version
+      content_type 'image/png'
+      image.format = 'png'
+      image.to_blob
+    else
+      pixels = image.rotate(180).negate.export_pixels(0, 0, WIDTH, HEIGHT, 'I')
+      interlace(pixels)
+    end
+  end
+
   get '/:agent/text.?:format?' do
     #agent url, text
     msg = params[:msg] || 'hello world'
@@ -83,7 +144,7 @@ class XbmApp < Sinatra::Base
     pixels = image.rotate(180).negate.export_pixels(0, 0, WIDTH, HEIGHT, 'I')
 
     options = {
-      :body => interlace(pixels)
+      body: interlace(pixels)
     }
 
     HTTParty.post(agent_url, options)
