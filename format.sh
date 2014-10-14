7e84efafcf324feac9a2b1cee12621569286ab50
Eric Betts <bettse@gmail.com>
2014-10-13 17:57:37 -0700

    format image into center and extent

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-convert -resize 264x176 -colorspace Gray -rotate 180 -negate $1 $1.xbm
+convert -resize 264x176 -colorspace Gray -gravity center -background white -extent 264x176 -rotate 180 "$1" "${1/png/xbm}"
+convert "${1/png/xbm}" preview.png && open preview.png
+
